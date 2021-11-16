1eaf5ffd776c5fca118d1d0ae41d7bc597d39d0c
manasm11 <53897110+manasm11@users.noreply.github.com>
2021-11-17 01:27:38 +0530

    Update WardrobeScreen.js

--- frontend/src/screens/WardrobeScreen.js
+++ frontend/src/screens/WardrobeScreen.js
@@ -27,33 +27,30 @@ const WardrobeScreen = ({ match }) => {
   const BITLY_URL = "https://api-ssl.bitly.com/v4/shorten"
   const BITLY_API_TOKEN = ""
 
-  const shortenUrl = async () => {
-    const myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json')
-    myHeaders.append('Authorization', BITLY_API_TOKEN)
-
-    let currentUrl = window.location.href;
-    let res = await fetch(BITLY_URL, {
+  async function shortenUrl() {
+    const res = await fetch(BITLY_URL, {
       method: 'POST',
-      body: {
-        "long_url": currentUrl,
-
-      }
-    })
-    console.log(res)
-    let shortenedUrl = res.body["link"]
-    return shortenedUrl
+      headers: {
+        'Authorization': `Bearer ${BITLY_API_TOKEN}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ "long_url": window.location.href, "domain": "bit.ly", "group_guid": "BlbgglkGzHr" })
+    });
+    const data = await res.json()
+    console.log(data)
+    navigator.clipboard.writeText(data['link'])
+    return 
   }
-  
+
   const clickHandler = () => {
-    let smallUrl = shortenUrl() || window.location.href
-    navigator.clipboard.writeText(smallUrl)
-    setText('Copied Link!')
-    setColor('success')
-    setTimeout(() => {
-      setColor('dark')
-      setText('Copy Shareable Link!')
-    }, 3000)
+    shortenUrl().then(() => {
+      setText('Copied Link!')
+      setColor('success')
+      setTimeout(() => {
+        setColor('dark')
+        setText('Copy Shareable Link!')
+      }, 3000)
+    })
   }
 
   return (
@@ -98,18 +95,6 @@ const WardrobeScreen = ({ match }) => {
             variant={color}
             onClick={clickHandler}
           > {text}</Button>
-          <Button
-            type='button'
-            variant='light'
-          >
-            <Link to={`/dashboard`}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-journal-richtext" viewBox="0 0 16 16">
-                <path d="M7.5 3.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0zm-.861 1.542 1.33.886 1.854-1.855a.25.25 0 0 1 .289-.047L11 4.75V7a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 7v-.5s1.54-1.274 1.639-1.208zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z" />
-                <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z" />
-                <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z" />
-              </svg>
-            </Link>
-          </Button>
         </Col>
       </Row>
     </div>
